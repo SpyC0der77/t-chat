fdd109a7232e1e132077b5571f018a2d4b78be79
ofcljaved <javedofficialme@gmail.com>
2025-06-12 18:20:03 +0530

    add react router

--- src/frontend/app.tsx
+++ src/frontend/app.tsx
@@ -1,3 +1,12 @@
+import { Route, BrowserRouter, Routes } from 'react-router';
+
 export default function App() {
-  return <div>Hello world</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Hello World</h1>} />
+        <Route path="/about" element={<h1>About</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
